f1a35832f667ad22eff753b42a32780adaf4ed1d
Gwamaka Charles <gwaxart19@gmail.com>
2020-08-26 15:23:23 +0300

    add all

--- App.js
+++ App.js
@@ -1,14 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import  MapView from "react-native-maps";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default class App extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.initialRegion = {
+//       latitude: 37.78825,
+//       longitude: -122.4324,
+//       latitudeDelta: 0.00922,
+//       longitudeDelta: 0.00421,
+//     };
+//   }
+
+//   render(){
+//     return(
+//       <View style={{flex:1}}>
+//         <MapView
+//         style = {styles.fullScreenMap}
+//         provider="google"
+//         initialRegion={this.initialRegion}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   fullScreenMap : {
+//     position: 'absolute',
+//     top:0,
+//     bottom:0,
+//     left:0,
+//     right:0,
+
+// },
+// }
+// );
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.initialRegion = {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.00922,
+      longitudeDelta: 0.00421,
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView 
+        provider="google"
+        style={styles.mapStyle}
+        initialRegion={this.initialRegion}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +68,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
